bd4438c355c4b5b94d85bbbd19da198c79f82ed5
Dave Laing <dave.laing.80@gmail.com>
2017-12-15 11:05:07 +1000

    Reworks the examples

--- final-bp-example-eval/Example/Eval.hs
+++ final-bp-example-eval/Example/Eval.hs
@@ -1,13 +1,6 @@
-module Example.Eval where
+module Example.Eval (
+   module X
+  ) where
 
-import Example.Base
-import Example.Mul
-
-eval1 :: Int
-eval1 = term1
-
-eval2 :: Int
-eval2 = term2
-
-eval3 :: Int
-eval3 = term3
+import Example.Base as X
+import Example.Mul as X
--- final-bp-example-print/Example/Print.hs
+++ final-bp-example-print/Example/Print.hs
@@ -1,13 +1,6 @@
-module Example.Print where
+module Example.Print (
+    module X
+  ) where
 
-import Example.Base
-import Example.Mul
-
-pr1 :: String
-pr1 = term1
-
-pr2 :: String
-pr2 = term2
-
-pr3 :: String
-pr3 = term3
+import Example.Base as X
+import Example.Mul as X
