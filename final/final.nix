17c927cd29b6e80382f26cfe75dd239788ceb6c4
Dave Laing <dave.laing.80@gmail.com>
2018-03-15 11:48:58 +1000

    Removes the nix files since we aren't using them

--- final/final.nix
+++
@@ -1,8 +0,0 @@
-{ mkDerivation, base, stdenv }:
-mkDerivation {
-  pname = "final";
-  version = "0.1.0.0";
-  src = ./.;
-  libraryHaskellDepends = [ base ];
-  license = stdenv.lib.licenses.bsd3;
-}
--- initial-fuse/initial.nix
+++
@@ -1,9 +0,0 @@
-{ mkDerivation, base, criterion, lens, stdenv }:
-mkDerivation {
-  pname = "initial";
-  version = "0.1.0.0";
-  src = ./.;
-  libraryHaskellDepends = [ base lens ];
-  benchmarkHaskellDepends = [ base criterion ];
-  license = stdenv.lib.licenses.bsd3;
-}
